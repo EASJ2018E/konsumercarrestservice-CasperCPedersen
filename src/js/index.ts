6dd6aee06f1b6597f4983f6b38f5dcefb80d4c57
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-10-24 11:26:19 +0200

    I did well. get done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,31 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, 
+{AxiosResponse, AxiosError}
+from "../../node_modules/axios/index";
+
+interface icar {
+    model : string;
+    vendor : string;
+    price : number;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let buttonElement: HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAll");
+buttonElement.addEventListener("click",()=> showAllCars())
+let div: HTMLDivElement = <HTMLDivElement> document.getElementById("content")
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function showAllCars() : void
+{
+    let uri : string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars";
+
+    axios.get<icar[]>(uri)
+    .then(function (response:AxiosResponse<icar[]>):void{
+        let result : string = "<ol>";
+        response.data.forEach((car : icar) => {
+            result += "<li>"+car.model + car.vendor + car.price.toString()+ "</li>";
+        });
+        result += "</ol>"
+        div.innerHTML = result;
+    })
+    .catch(function (error:AxiosError):void{
+        div.innerHTML = error.code += error;
+    })
+}
